9e678b8b2dd48446443caf170455cccb18c86f1f
arman0824 <armanushendrapratapsingh@gmail.com>
2026-09-18 12:21:21 +0530

    switches and while loop

--- c/calc.c
+++ c/calc.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
     int num1, num2, inp;
     float result;
 
     printf("Choose operation\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication\n");
-    printf("Enter choice number; ");
-    scanf("%d", &inp);
 
-    if (inp > 5 || inp < 1){
-        printf("Error Incorrect choice number\n");
-        return 1;
-    }
-    else{
-        printf("Enter value for number 1: ");
-        scanf("%d", &num1);
-        
-        printf("Enter value for number 2: ");
-        scanf("%d", &num2);
+    while(true)
+    {
+        printf("Enter choice number: ");
+        scanf("%d", &inp);
+
+        if (inp > 4 || inp < 1){
+            printf("Error Incorrect choice number\n");
+        }
+        else{
+            printf("Enter value for number 1: ");
+            scanf("%d", &num1);
+            
+            printf("Enter value for number 2: ");
+            scanf("%d", &num2);
+            break;
+        }
     }
 
-    if (inp == 1){
+    switch (inp)
+    {
+    case 1:
         result = num1 + num2;
-    }
-    if (inp == 2){
+        break;
+    case 2:
         result = num1 - num2;
-    }
-    if (inp == 3){
+        break;
+    case 3:
         result = num1 / num2;
-    }
-    if (inp == 4){
+        break;
+    case 4:
         result = num1 * num2;
-        return 0;
+        break;
     }
     printf("The Result Is: %.2f\n", result);
 }
