9bcd1b87f2e91d4e415bb186a9c6f780cd3fb5cd
arman0824 <armanushendrapratapsingh@gmail.com>
2026-09-19 15:52:01 +0530

    rock paper scissors in c

---
+++ c/rock_paper_scissors.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand(time(NULL));
+
+    int comp = ((rand() % 3) + 1), user;
+
+    printf("***ROCK PAPER SCISSORS***\nSelect your MOVE(1-3): ");
+    scanf("%d", &user);
+
+    while (comp == 1)
+    {
+        switch (user)
+        {
+        case 1:
+            printf("ITS A TIE\n");
+            break;
+        case 2:
+            printf("USER WINS\n");
+            break;
+        case 3:
+            printf("COMPUTER WINS\n");
+            break;
+        }
+        break;
+        
+    }
+    while (comp == 2)
+    {
+        switch (user)
+        {
+        case 1:
+            printf("COMPUTER WINS\n");
+            break;
+        case 2:
+            printf("ITS A TIE\n");
+            break;
+        case 3:
+            printf("USER WINS\n");
+            break;
+        }
+        break;
+        
+    }
+    while (comp == 3)
+    {
+        switch (user)
+        {
+        case 1:
+            printf("USER WINS\n");
+            break;
+        case 2:
+            printf("COMPUTER WINS\n");
+            break;
+        case 3:
+            printf("ITS A TIE\n");
+            break;
+        }
+        break;
+    }
+    switch (user)
+    {
+    case 1:
+        printf("You picked ROCK\n");
+        break;
+    case 2:
+        printf("You picked PAPER\n");
+        break;
+    case 3:
+        printf("You picked SCISSORS\n");
+        break;
+    }
+    switch (comp)
+    {
+    case 1:
+        printf("COMPUTER picked ROCK\n");
+        break;
+    case 2:
+        printf("COMPUTER picked PAPER\n");
+        break;
+    case 3:
+        printf("COMPUTER picked SCISSORS\n");
+        break;
+    }
+}
