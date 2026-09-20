6a1ecaeb646ab420d0b6a459926b9bfe9f1887ab
arman0824 <armanushendrapratapsingh@gmail.com>
2026-09-20 13:11:34 +0530

    Fix gitignore: remove tracked files that should be ignored

---
+++ c/banking.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main(){
+
+    double balance=0, deposit=0, withdraw=0;
+    char user[20] = "";
+
+    printf("***WELCOME TO CITY BANK");
+    printf("Enter your name to continue: ");
+}
